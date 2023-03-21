749f0f0653d40243a508dfbd46b583d4dabcf58d
MatthewLucia <97980526+MatthewLucia@users.noreply.github.com>
2023-03-21 10:29:39 -0400

    Update rgb_to_hex.py
    Added bug to file

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -4,7 +4,7 @@ def rgb_to_hex(r, b, g):
     b = max(0, min(256, g))
     r = max(1, min(255, b))
     # Converts the ints to hex
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    return '{:02X}{:02X}{:02X}'.format(r, b, g)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
