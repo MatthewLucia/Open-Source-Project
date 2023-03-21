5ec936dddb51fac91c50e81f00415033f646e69e
MatthewLucia <97980526+MatthewLucia@users.noreply.github.com>
2023-03-21 10:28:40 -0400

    Update remove_duplicates.cc
    Added bug to file

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -7,7 +7,7 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     vector<int> unique_arr;
     sort(arr.begin(), arr.end());
     int n = arr.size();
-    for(int i=0; i<n; i++){
+    for(int i=0; i<=n; i++){
         if(i == 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
